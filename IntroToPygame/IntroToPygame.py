d6aa8b75734097ef35734d063c00354ed0b17c34
Sam Layton <slayton@uccs.edu>
2019-02-23 20:00:12 -0700

    Added debugging constants

--- IntroToPygame/IntroToPygame.py
+++ IntroToPygame/IntroToPygame.py
@@ -41,7 +41,7 @@ myDog = Dog(Vector(Constants.WORLD_WIDTH / 2, Constants.WORLD_HEIGHT / 2), Vecto
 
 # spawn 10 sheep at random points on map
 sheep = []
-for i in range(30):
+for i in range(50):
 	randX = random.randint(1, Constants.WORLD_WIDTH - Constants.SHEEP_WIDTH - 1)
 	randY = random.randint(1, Constants.WORLD_HEIGHT - Constants.SHEEP_HEIGHT - 1)
 	newSheep = Sheep(Vector(randX, randY), Vector(Constants.SHEEP_WIDTH, Constants.SHEEP_HEIGHT), Constants.ENEMY_SPEED, Constants.ENEMY_COLOR, sheepSurface)
